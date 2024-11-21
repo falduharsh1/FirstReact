2c39ae6f7407494177136a3a00d49ebefa003e74
falduharsh1 <falduharsh1@gmail.com>
2024-11-21 08:45:24 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (File Structure)
+          Learn React (Demo)
         </a>
         
       </header>
