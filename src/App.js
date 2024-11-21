db5471db86449c58db09abad0f3edd62385e915b
falduharsh1 <falduharsh1@gmail.com>
2024-11-21 08:30:33 +0530

    File_structure

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (File Structure)
         </a>
         
       </header>
