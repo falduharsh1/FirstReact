206bc4700522fe2795b77f2a9b40f116880088ae
falduharsh1 <falduharsh1@gmail.com>
2024-11-22 08:31:32 +0530

    Component

---
+++ src/Component/MyCountry.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class MyCountry extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Class Based Component</h2>
+                <div>MyCountry</div>
+            </div>
+        );
+    }
+}
+
