8368317ef4b238f35c72a9c764a1a528937c8cc2
falduharsh1 <falduharsh1@gmail.com>
2024-11-22 09:20:46 +0530

    change

--- src/Component/MyCountry.js
+++ src/Component/MyCountry.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 export default class MyCountry extends Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            name : 'India',
+            Population : '142 Billion'
+        }
+    }
+
+    HandleCounty = () => {
+        this.setState({
+             name : 'USA',
+            Population : '14 Billion'
+        })
+    }
+    
     render() {
         return (
             <div>
                 <h2>Class Based Component</h2>
-                <div>MyCountry</div>
+                <div>MyCountry : {this.state.name}</div>
+                <div>Population : {this.state.Population}</div>
+                <button onClick={this.HandleCounty}>Change Country</button>
             </div>
         );
     }
---
+++ src/Component/MyCountryFun.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function MyCountryFun () {
+    return (
+        <div>
+            <h2>Function Based Component</h2>
+            <div>MyCountryFun</div>
+        </div>
+    );
+}
+
