41a8a6eb6498baccab5cda4eb47974ffd99fe927
falduharsh1 <falduharsh1@gmail.com>
2024-11-23 09:22:36 +0530

    23_11_24_fun_compo

--- src/Component/MyCountryFun.js
+++ src/Component/MyCountryFun.js
@@ -1,23 +1,37 @@
 import React, { useState } from "react";
 
-export default function MyCountryFun() {
-  const [Country, setCountry] = useState({
-    name: "India",
-    Population: "142 Billion",
-  });
+// export default function MyCountryFun() {
+//   const [Name, setName] = useState("INDIA");
+//   const [Population, setPopulation] = useState("135 Billion");
+
+  // const HandleCounty = () => {
+  //   setName("USA");
+  //   setPopulation("15 Billion");
+  //   setShow(!show);
+  //   if (Name === "INDIA") {
+  //     setName("USA");
+  //     setPopulation("15 Billion");
+  //   } else {
+  //     setName("INDIA");
+  //     setPopulation("135 Billion");
+  //   }
+  // };
+
+  export default function MyCountryFun() {
+    const [Name, setName] = useState(true);
+    const [Population, setPopulation] = useState(true);
+
 
   const HandleCounty = () => {
-    setCountry({
-        name : 'USA',
-        Population : '14 Billion'
-    })
+    setName(!Name);
+    setPopulation(!Population)
   }
 
   return (
     <div>
       <h2>Function Based Component</h2>
-      <div>MyCountryFun : {Country.name}</div>
-      <div>Population : {Country.Population}</div>
+      <div>MyCountryFun : {Name ? 'INDIA' : 'USA'}</div>
+      <div>Population : {Population ? '135 Billion' : '15 Billion'}</div>
       <br></br>
       <button onClick={HandleCounty}>Change Country</button>
     </div>
