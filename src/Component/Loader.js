4e2bd07fe76117eb817d7e2d31dbce0cf17f42e2
falduharsh1 <falduharsh1@gmail.com>
2024-12-05 09:02:29 +0530

    cat_complete

--- src/Component/Loader.js
+++ src/Component/Loader.js
@@ -4,12 +4,25 @@ export default function Loader() {
   const [Loading, setLoading] = useState(true);
   const [datas, setdatas] = useState([]);
   const [search, setsearch] = useState("");
-  const [sort , setsort] = useState("");
-  const [category , setcategory] = useState('')
+  const [sort, setsort] = useState("");
+  const [category, setcategory] = useState("");
+  const [Loader_data , set_Loader_data] = useState([]);
+  const [allData , set_allData] = useState('')
+  const [bgColor, setBgColor] = useState("white");  
 
   const GetData = async () => {
     const response = await fetch("https://fakestoreapi.com/products");
     const data = await response.json();
+
+    let uniquearr = [];
+
+     data.map((v) => {
+      if (!uniquearr.includes(v.category)) {  
+        uniquearr.push(v.category);
+      }
+      set_Loader_data(uniquearr)
+    });
+
     setdatas(data);
     setLoading(false);
   };
@@ -18,46 +31,35 @@ export default function Loader() {
 
   const HandleFiltering = () => {
     console.log(search);
-    let Fdata = []
+    let Fdata = [];
 
-    const F_data = datas.filter((v, i) => (
-      v.title.toLowerCase().includes(search.toLowerCase()) ||
-        v.price.toString().includes(search.toString()) || 
+    const F_data = datas.filter(
+      (v, i) =>
+        v.title.toLowerCase().includes(search.toLowerCase()) ||
+        v.price.toString().includes(search.toString()) ||
         v.category.toLowerCase().includes(search.toLowerCase()) ||
         v.description.toLowerCase().includes(search.toLowerCase())
-    ));
+    );
 
-    if(sort === 'l_h'){
-      F_data.sort((a,b) => a.price - b.price)
-    }else if(sort === 'h_l'){
-      F_data.sort((a,b) => b.price - a.price)
-    }else if(sort === 'a_z'){
-      F_data.sort((a,b) => a.title.localeCompare(b.title))
-    }else if(sort === 'z_a'){
-      F_data.sort((a,b) => b.title.localeCompare(a.title))
+    if (sort === "l_h") {
+      F_data.sort((a, b) => a.price - b.price);
+    } else if (sort === "h_l") {
+      F_data.sort((a, b) => b.price - a.price);
+    } else if (sort === "a_z") {
+      F_data.sort((a, b) => a.title.localeCompare(b.title));
+    } else if (sort === "z_a") {
+      F_data.sort((a, b) => b.title.localeCompare(a.title));
     }
 
-    // if(F_data != categoryes){
-    //     return F_data
-    // }
+    if (category) {
+      return F_data.filter((v) => v.category === category);
+    }
     return F_data;
   };
 
   console.log();
-  
 
   const Final_Data = HandleFiltering();
-  let uniquearr =[]
-
-  let categoryes = Final_Data.map((v => v.category))
-  console.log(categoryes);
-  
-  let u_data = categoryes.map((v) => {
-    if(!uniquearr.includes(v)){
-      uniquearr.push(v)
-    }
-  })
-
   console.log(Final_Data);
 
   useEffect(() => {
@@ -65,61 +67,70 @@ export default function Loader() {
     // }, 3000);
     setLoading(false);
     GetData();
- }, []);
+  }, []);
 
- console.log("uniquearr", category);
+  console.log("uniquearr", category);
 
-return (
-  <div>
-    <div style={{
-      display : "flex",
-      justifyContent: "center",
-      alignItems: "center"
-    }}>
-      <input
-        type="text"
-        name="search"
-        id="Input"
-        placeholder="Search here..."
-        onChange={(e) => setsearch(e.target.value)}
-      />
-      <select id="selection"
-        onChange={(e) => setsort(e.target.value)}
+   const toggleBackground = () => {
+    if (bgColor === 'white') setBgColor('black');
+      if (bgColor === 'black') setBgColor('white');
+   }
+
+  return (
+    
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <option value="0">---Select---</option>
-        <option value="l_h">Lower to High</option>
-        <option value="h_l">High to Lower</option>
-        <option value="a_z">a to z</option>
-        <option value="z_a">z to a</option>
-      </select>
+        <input
+          type="text"
+          name="search"
+          id="Input"
+          placeholder="Search here..."
+          onChange={(e) => setsearch(e.target.value)}
+        />
+        <select id="selection" onChange={(e) => setsort(e.target.value)}>
+          <option value="0">---Select---</option>
+          <option value="l_h">Lower to High</option>
+          <option value="h_l">High to Lower</option>
+          <option value="a_z">a to z</option>
+          <option value="z_a">z to a</option>
+        </select>
 
-    {
-      uniquearr.map((v) => (
-        <button className="cat_btn" onClick={() => setcategory(v)}>
-          {v}
-        </button>
-      ))
-    }
+        <button  className="cat_btn" onClick={() => {setcategory("")}}>All</button>
 
-
-    </div>
-
-    {Loading ? (
-      <div className="loader">Loading...</div>
-    ) : (
-      <div className="row">
-        {Final_Data.map((v, i) => (
-          <div className="col-4">
-            <h2>{v.title}</h2>
-            <img src={v.image} width="150px" height="170px" alt="Product" />
-            <h2>{v.price}</h2>
-            <h3>{v.category}</h3>
-            <p>{v.description}</p>
-          </div>
+        {Loader_data.map((v) => (
+          <button
+            className="cat_btn"
+            onClick={() => {
+              setcategory(v);
+              toggleBackground()
+            }}
+          >
+            {v}
+          </button>
         ))}
       </div>
-    )}
-  </div>
-);
- }
 
+      {Loading ? (
+        <div className="loader">Loading...</div>
+      ) : (
+        <div className="row">
+          {Final_Data.map((v, i) => (
+            <div className="col-4">
+              <h2>{v.title}</h2>
+              <img src={v.image} width="150px" height="170px" alt="Product" />
+              <h2>{v.price}</h2>
+              <h3>{v.category}</h3>
+              <p>{v.description}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
